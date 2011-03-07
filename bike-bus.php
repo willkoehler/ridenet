c572256cf66756283831a97bf361a83c126f65f9
Will Koehler <wck555@gmail.com>
2011-03-07 12:16:10 -0500

    changed title of bike bus page

--- bike-bus.php
+++ bike-bus.php
@@ -14,7 +14,7 @@ $RideBoardLength = 30;
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <title><?BuildPageTitle($oDB, 0, "Commuting Home")?></title>
+  <title><?BuildPageTitle($oDB, 0, "Bike Bus")?></title>
 <!-- Include common code and stylesheets -->
   <? IncludeExtJSFiles() ?>
 <!-- Include site stylesheets -->
