40df5fa5fcc86cfc8b37ddbe7bc36ac5c3748ca7
Will Koehler <wck555@gmail.com>
2015-07-02 10:22:16 -0400

    Remove reference to pear.php
    We no longer use the Pear PHP libraries.

--- playpen/ServerInfo.php
+++ playpen/ServerInfo.php
@@ -7,10 +7,4 @@ phpinfo();
 // phpinfo(8) yields identical results.
 phpinfo(INFO_MODULES);
 
-if (require 'PEAR.php') {
-    echo "You can now use PEAR with PHP!";
-} else {
-    echo "PEAR is not recognized with your PHP.";
-}
-
 ?>
